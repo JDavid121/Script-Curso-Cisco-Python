77ba550a813806d769d4adccff87db2bc62af9b0
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-03-29 15:01:11 -0500

    Add files via upload
    Programa del área de un círculo.

---
+++ 02 Area de un circulo.py
@@ -0,0 +1,28 @@
+
+#Programa que calcula el área de un circulo
+"""
+Programa que utliza el radio del círculo para determinar su área
+"""
+print(" ")
+print(" ")
+print(" ")
+print("PROGRAMA PARA DETERMINAR EL ÁREA DE UN CÍRCULO")
+print(" ")
+print(" ")
+print("A=r2*PI")
+print(" ")
+print(" ")
+r=input("Ingresar el radio del círculo en m  ")
+print(" ")
+print(" ")
+print ("El radio del circulo es   ",r,"m")
+PI=3.14159 #Constante PI
+R=float(r) #Transformamos r de tipo str a tipo float
+A=(R**2)*PI #calculamos el área del círculo
+print(" ")
+print(" ")
+print("El área del círculo es   ",A,"m2")
+print(" ")
+print(" ")
+print("Fin del programa")
+
