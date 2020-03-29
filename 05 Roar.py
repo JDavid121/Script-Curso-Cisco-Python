65e3d6fcdba64f18237e2d1e196a875407c04db0
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-03-29 15:03:12 -0500

    Add files via upload
    Curso Básico Python CISCO. Roar

---
+++ 05 Roar.py
@@ -0,0 +1,12 @@
+"""
+Programa que crea el rugido de un león
+
+"""
+print(" ")
+print(" ")
+print("ROAR...") #Enseña en la pantalla el rugido del León
+print(" ")
+print(" ")
+print("Fin de la Pantalla")
+
+print("***************************************************")
